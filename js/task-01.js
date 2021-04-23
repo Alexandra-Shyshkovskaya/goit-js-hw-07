f4b9261f2adbfabe63896f1ae8227414e293e7d8
Alexandra-Shyshkovskaya <74681434+Alexandra-Shyshkovskaya@users.noreply.github.com>
2021-04-23 21:20:03 +0300

    first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,15 @@
 //Напиши скрипт, который выполнит следующие операции.
-//Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. 
+//1.Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. 
 //Получится 'В списке 3 категории.'.
-//Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2)
+//2.Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2)
 //и количество элементов в категории(всех вложенных в него элементов li).
 //Например для первой категории получится:
 //Категория: Животные
 //Количество элементов: 4
 
-const quantityCategories =;
+const quantityCategories = document.querySelectorAll('.item');
+console.log(`В списке ${quantityCategories.length} категории.`);
+
+quantityCategories.forEach((el) =>
+    console.log(`Категория: ${el.firstElementChild.textContent}`,
+    `Количество элементов: ${el.lastElementChild.children.length}`));
